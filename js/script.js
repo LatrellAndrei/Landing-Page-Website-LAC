216179c016085d507ec86fe1895d27e75894d44d
Latrell Andrei Camurungan <l-c1639@students.uc-bcf.edu.ph>
2024-02-08 13:59:57 +0800

    new changes in the css mostly in responsive and html also

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
