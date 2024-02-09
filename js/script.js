8443b61af97815634d177f0ab3f1475c8b49e86b
Latrell Andrei Camurungan <l-c1639@students.uc-bcf.edu.ph>
2024-02-09 18:44:43 +0800

    update with the html and js auto type animation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// for the navbar menu icon when click
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
@@ -5,3 +6,11 @@ menu.onclick = () => {
   menu.classList.toggle("bx-x");
   navbar.classList.toggle("open");
 };
+
+// for auto typing
+var typed = new Typed(".heading__2", {
+  strings: ["Customers", "Business", "Teachers", "Students"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
